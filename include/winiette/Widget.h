5bafface30f8d7431165925d58f0424cd8801ed0
Milinkov Nikita <milinkov.nik@gmail.com>
2023-12-21 17:47:19 +0300

    Добавьте файлы проекта.

---
+++ include/winiette/Widget.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef WINIETTE_INCLUDE_WINIETTE_WIDGET_H_
+#define WINIETTE_INCLUDE_WINIETTE_WIDGET_H_
+
+#include <winiette/win32.h>
+
+class Widget
+{
+public:
+private:
+};
+
+#endif WINIETTE_INCLUDE_WINIETTE_WIDGET_H_
---
+++ include/winiette/win32.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef WINIETTE_INCLUDE_WINIETTE_WIN32_H_
+#define WINIETTE_INCLUDE_WINIETTE_WIN32_H_
+
+#include <sdkddkver.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <commdlg.h>
+
+#endif  // WINIETTE_INCLUDE_WINIETTE_WIN32_H_
